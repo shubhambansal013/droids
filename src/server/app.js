b4b618ddab5f04a1e63f1965357d666bd80d2c7e
shubham <shubham.bansal@jugnoo.in>
2017-01-06 17:01:16 +0530

    reading files using fs watch

---
+++ src/server/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var request = require('request');
+var morgan = require('morgan');
+var socket_io = require('socket.io');
+var bodyParser = require('body-parser');
+
+var logger = require('../logger');
+
+var app = express();
+
+app.set('port', process.env.PORT || 9000)
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit : '200mb'}));
+app.use(bodyParser.urlencoded({limit : '20mb', extended : true}));
+app.use(function (req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
+
+app.get('/hello', (req, res) => {
+  logger.info('GET : hello');
+  return res.send('whatup');
+});
+
+var listener = app.listen(process.env.PORT || 9000);
+var io = socket_io.listen(listener);
