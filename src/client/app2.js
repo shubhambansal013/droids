02065933ba5d54dc5712d0323980df39737bb18e
shubham <shubham.bansal@jugnoo.in>
2017-01-09 17:27:22 +0530

    added comments and client can define the data directory

--- src/client/app2.js
+++ src/client/app2.js
@@ -2,44 +2,22 @@ var io = require('socket.io-client');
 var path = require('path');
 var md5 = require('md5-file');
 var promise = require('bluebird');
+
 var fs = require('fs');
 var logging = require('../logger');
 
-var data_dir = path.join(__dirname, './data');
+
+var data_dir_default = path.join(__dirname, './data');
+
+var data_dir = (process.env.DATA_DIR, data_dir_default);
 
 promise.promisifyAll(fs);
-// promise.promisify(mapd5);
 
 var connection = io.connect('http://localhost:9000');
 
-// connection.emit('hi', {HAHA : 'hahaha'});
+// connection.emit('hi', {HEY : 'hello'});
+
 var data = {};
-function calculateHash(cb){
-		promise.coroutine(function *(){
-	  if(!data_dir){
-	    var err = new Error('No data exists');
-	    return err;
-	  }
-	  logging.info({DIRNAME : data_dir});
-	    var files = yield fs.readdirAsync.call(null, data_dir);
-	    var filesPath = files.map(function(file){
-	    	return path.join(data_dir, file);
-	    });
-	    for(var i = 0; i < filesPath.length; ++i){
-	    	var hash = yield promise.resolve(md5.sync(filesPath[i]));
-	    	data[hash] = files[i];
-	    }
-	    return data;
-	})()
-	.then(function(data){
-		logging.info({DATA : data});
-		return cb(null);
-	})
-	.catch(function(error){
-	  logging.error({ERROR : error.message});
-	  return cb(error);
-	})
-}
 
 connection.emit('addUser', process.env.USER);
 connection.emit('pair', process.env.PAIR);
@@ -50,7 +28,7 @@ calculateHash(function(error){
 		return logging.error(error);
 	}
 	connection.emit('storeHash', data);
-})
+});
 
 connection.on('sendFile', function(fileObj){
 	var files = {};
@@ -75,7 +53,7 @@ connection.on('receive', function(files){
 		logging.info('Sending storeHash Event');
 		connection.emit('storeHash', data);
 	});
-})
+});
 
 connection.on('complete', function(){
 	console.log('sync done');
@@ -86,6 +64,36 @@ connection.on('hashStored', function(){
 	logging.info('Hash stored on server');
 });
 
+connection.on('pairOnline', function(msg){
+	logging.info(msg);
+});
 connection.on('error', function(error){
 	logging.error(error);
-})
+});
+
+function calculateHash(cb){
+	promise.coroutine(function *(){
+		if(!data_dir){
+			var err = new Error('No data exists');
+			return err;
+		}
+		logging.info({DIRNAME : data_dir});
+		var files = yield fs.readdirAsync.call(null, data_dir);
+		var filesPath = files.map(function(file){
+			return path.join(data_dir, file);
+		});
+		for(var i = 0; i < filesPath.length; ++i){
+			var hash = yield promise.resolve(md5.sync(filesPath[i]));
+			data[hash] = files[i];
+		}
+		return data;
+	})()
+		.then(function(data){
+			logging.debug('DATA', data);
+			return cb(null);
+		})
+		.catch(function(error){
+			logging.error('ERROR', error.message);
+			return cb(error);
+		})
+}
--- src/server/routes/sync.js
+++
@@ -1,5 +0,0 @@
-var promise = require('bluebird');
-var md5 = require('md5-file');
-
-var exports = module.exports = {};
-
